f20a6604ddaf0ba5fbb793cb8358983bf0efa819
dave.holtzhouser <dave.holtzhouser@maritz.com>
2015-04-12 17:29:28 -0500

    plot3 updates

---
+++ plot3.R
@@ -0,0 +1,30 @@
+
+# Load the needed packages
+packages <- c("data.table", "lubridate", "datasets")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
+
+
+df <- fread("household_power_consumption.txt", skip = "1/2/2007", nrows = 2880, 
+            colClasses = c("DATE", "numeric", "numeric", "numeric", "numeric","numeric",
+                           "numeric","numeric","numeric"), sep = ";", na.string = "?")
+
+setnames(df, c("date", "time", "global_active_power", "global_reactive_power", "voltage", 
+               "global_intensity", "sub_metering_1", "sub_metering_2", "sub_metering_3"))
+
+df$DateTime <- dmy_hms(paste(df$date, df$time))
+
+png(file = "plot3.png", width = 480, height = 480)
+with(df, plot(df$DateTime, df$sub_metering_1, type= "l", 
+              xlab = "",
+              ylab = "Energy sub metering"))
+lines(df$DateTime, df$sub_metering_2, col = "red", type = "l")
+lines(df$DateTime, df$sub_metering_3, col = "blue", type = "l")
+legend("topright", pch = 1, col = c("black", "red", "blue"),
+        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+     
+
+dev.off()
+
+
+
